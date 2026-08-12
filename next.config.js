6dc80d222011b96eb320e6a7108f67c49b6a3fbf
prathomix-aicom <prathomix@gmail.com>
2026-08-12 22:23:35 +0530

    Fixed Vercel build encoding error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Don't attempt to load these server-side modules on the client
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        encoding: false,
-      };
-    }
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      encoding: false,
+    };
     return config;
   },
 };
